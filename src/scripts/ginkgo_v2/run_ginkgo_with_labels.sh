b6f34c140df393cfb728ee59299719fcc4d0d0aa
Nick Armstrong <nick.armstrong@helix.com>
2023-06-12 16:47:33 -0700

    replace flag with label in ginkgo-v2-labels to clarify difference between this and existing jobs

---
+++ src/scripts/ginkgo_v2/run_ginkgo_with_labels.sh
@@ -0,0 +1,36 @@
+if [[ "${GINKGO_PARAMETERS}" == *"-label-filters"* ]]; then
+  echo "please set label-filters using label_filter_override"
+  exit 1
+fi
+
+if [ -d test-results ]; then
+  rm -rf test-results
+fi
+mkdir test-results
+
+HAS_LABEL_FILTERS=false
+FILTER_LABELS=""
+if [[ -n ${LABEL_FILTER_OVERRIDE} ]]; then
+  echo "using label filters found in label_filter_override"
+  HAS_LABEL_FILTERS=true
+  FILTER_LABELS="${LABEL_FILTER_OVERRIDE}"
+fi
+
+if [[ -n ${TEST_TYPE} ]]; then
+  echo "using focus filters found in test_type"
+  if [[ ${HAS_LABEL_FILTERS} == true ]]; then
+    echo "ERROR: -label-filters are already set somewhere else"
+    exit 1
+  fi
+  HAS_LABEL_FILTERS=true
+
+  FILTER_LABELS="${TEST_TYPE} && !skip-${ENVIRONMENT}"
+fi
+
+if [[ ${HAS_LABEL_FILTERS} == false ]]; then
+  echo "no label filters found: please set label filters with test_type or label_filter_override."
+fi
+
+echo "running ginkgo with label filters: ${FILTER_LABELS}"
+ginkgo version
+ginkgo "${GINKGO_PARAMETERS}" -label-filter="${FILTER_LABELS}"
