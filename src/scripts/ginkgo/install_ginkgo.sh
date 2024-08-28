8c24b17955e0ea9db2e6b96705bcf48e7ffd1361
Nick Armstrong <nick.armstrong@helix.com>
2024-08-28 09:22:00 -0700

    fixing GO111MODULE env

--- src/scripts/ginkgo/install_ginkgo.sh
+++ src/scripts/ginkgo/install_ginkgo.sh
@@ -1,5 +1,5 @@
 echo "Installing ginkgo v2"
-GO111MODULE=on
+
 
 MOD_GINKGO_RAW=$(go mod edit -json 2> /dev/null | jq '.Require[] | select(.Path | test("^github.com/onsi/ginkgo.*$")) | {(.Path|tostring) : .Version}' -c | sort -t ':' -k2 -Vr | head -n 1)
 
@@ -10,7 +10,7 @@ if [ -n "$MOD_GINKGO_RAW" ]; then
     INSTALL_GINKGO_VERSION="$("$GOPATH"/bin/ginkgo version 2> /dev/null | awk '{ print "v"$3 }')"
     if [ ! "$MOD_GINKGO_VERSION" = "$INSTALL_GINKGO_VERSION" ]; then
         echo "installing $MOD_GINKGO_GO_STRING"
-        go install "$MOD_GINKGO_GO_STRING"
+        GO111MODULE=on go install "$MOD_GINKGO_GO_STRING"
     fi
 fi
 
