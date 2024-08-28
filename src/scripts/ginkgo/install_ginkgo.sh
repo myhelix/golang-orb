54c736aeff04bf08441ffe5f33be7191a7c58e87
Nick Armstrong <nick.armstrong@helix.com>
2024-08-28 09:20:04 -0700

    updating ginkgo install to use the ginkgo install script

---
+++ src/scripts/ginkgo/install_ginkgo.sh
@@ -0,0 +1,17 @@
+echo "Installing ginkgo v2"
+GO111MODULE=on
+
+MOD_GINKGO_RAW=$(go mod edit -json 2> /dev/null | jq '.Require[] | select(.Path | test("^github.com/onsi/ginkgo.*$")) | {(.Path|tostring) : .Version}' -c | sort -t ':' -k2 -Vr | head -n 1)
+
+if [ -n "$MOD_GINKGO_RAW" ]; then
+    MOD_GINKGO_GO_STRING=$(echo "$MOD_GINKGO_RAW" | jq -r 'to_entries[] | "\(.key)/ginkgo@\(.value)"')
+    MOD_GINKGO_VERSION=$(echo "$MOD_GINKGO_RAW" | jq -r 'to_entries[] | "\(.value)"')
+
+    INSTALL_GINKGO_VERSION="$("$GOPATH"/bin/ginkgo version 2> /dev/null | awk '{ print "v"$3 }')"
+    if [ ! "$MOD_GINKGO_VERSION" = "$INSTALL_GINKGO_VERSION" ]; then
+        echo "installing $MOD_GINKGO_GO_STRING"
+        go install "$MOD_GINKGO_GO_STRING"
+    fi
+fi
+
+goenv version
--- src/scripts/ginkgo_v2/install_ginkgo_v2.sh
+++
@@ -1,4 +0,0 @@
-which ginkgo
-echo "Installing ginkgo v2"
-GO111MODULE=on go install github.com/onsi/ginkgo/v2/ginkgo@latest
-goenv version
