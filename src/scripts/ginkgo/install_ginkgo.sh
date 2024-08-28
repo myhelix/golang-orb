adb001e12d57ac90278faa5d998d797498b26ff4
Nick Armstrong <nick.armstrong@helix.com>
2024-08-28 09:55:55 -0700

    simplifying

--- src/scripts/ginkgo/install_ginkgo.sh
+++ src/scripts/ginkgo/install_ginkgo.sh
@@ -4,14 +4,9 @@ echo "Installing ginkgo"
 MOD_GINKGO_RAW=$(go mod edit -json 2> /dev/null | jq '.Require[] | select(.Path | test("^github.com/onsi/ginkgo.*$")) | {(.Path|tostring) : .Version}' -c | sort -t ':' -k2 -Vr | head -n 1)
 
 if [ -n "$MOD_GINKGO_RAW" ]; then
-    MOD_GINKGO_GO_STRING=$(echo "$MOD_GINKGO_RAW" | jq -r 'to_entries[] | "\(.key)/ginkgo@\(.value)"')
-    MOD_GINKGO_VERSION=$(echo "$MOD_GINKGO_RAW" | jq -r 'to_entries[] | "\(.value)"')
-
-    INSTALL_GINKGO_VERSION="$("$GOPATH"/bin/ginkgo version 2> /dev/null | awk '{ print "v"$3 }')"
-    if [ ! "$MOD_GINKGO_VERSION" = "$INSTALL_GINKGO_VERSION" ]; then
+    MOD_GINKGO_GO_STRING=$(echo "$MOD_GINKGO_RAW" | jq -r 'to_entries[] | "\(.key)/ginkgo"')
         echo "installing $MOD_GINKGO_GO_STRING"
         GO111MODULE=on go install "$MOD_GINKGO_GO_STRING"
-    fi
 fi
 
 goenv version
